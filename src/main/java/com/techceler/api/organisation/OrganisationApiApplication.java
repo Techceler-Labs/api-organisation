6af6f460e5e9965e776c5d89d38b62bc9822fe4a
Hammad Afridi <hammad.afridi@contento3.com>
2017-11-29 13:03:09 +0500

    added comments

--- src/main/java/com/techceler/api/organisation/OrganisationApiApplication.java
+++ src/main/java/com/techceler/api/organisation/OrganisationApiApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * Starter class for organisation API
+ * @author hamakhaa
+ *
+ */
 @SpringBootApplication
 public class OrganisationApiApplication extends SpringBootServletInitializer {
 
